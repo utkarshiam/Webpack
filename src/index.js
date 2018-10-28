3966851c8605524f9a370a4198e38f2efd0d2882
WEIRDLEMON <utkarshdhy@gmail.com>
2018-10-28 18:46:41 +0530

    react included

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-const friends =['Anne','Bill','Cathy','KOOTAH','HAI','ARYan']
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const friends =['Anne','Bill','Cathy']
 console.log(friends);
 
 var double= x =>2*x;
 console.log(double(5));
+
+const App= () => <h1>Hello</h1>;
+ReactDOM.render(<App/>, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,18 @@ module.exports = {
     filename:'bundle.js',
     path:path.join(__dirname,'build')
   },
+  module:{
+    rules:[{
+      test:/\.js$/,
+      exclude:/(node_modules)/,
+      use:{
+        loader:'babel-loader',
+        options:{
+          presets:['env','react']
+        }
+      }
+    }]
+  },
   devServer:{
     historyApiFallback:true,
     contentBase:"./",
